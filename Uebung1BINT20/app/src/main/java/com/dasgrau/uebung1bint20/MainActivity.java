e9dd8aa4149ea9d2541d256ad6d89a1cafa24caa
Jörg Daubert <kannx@kannx.net>
2023-01-04 11:53:12 +0100

    Beispiellösung Übung 1

---
+++ Uebung1BINT20/app/src/main/java/com/dasgrau/uebung1bint20/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dasgrau.uebung1bint20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.toString();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "In onCreate");
+    }
+}
